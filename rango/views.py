313495d742087d6801b263e1c977401429e45ed4
2790004z <2790004z@student.gla.ac.uk>
2023-01-29 22:03:23 +0000

    chapter

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-"""def index(request):
+def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!', 'categories': category_list}
@@ -38,13 +38,6 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
 
-    return render(request, 'rango/category.html', context=context_dict)"""
-
-def index(request):
-    return HttpResponse("Rango says hey there partner!\n <a href='/rango/about/'>About</a>")
-
-
-def about(request):
-    return HttpResponse("Rango says here is the about page.\n <a href='/rango/'>Index</a>")
+    return render(request, 'rango/category.html', context=context_dict)
 
 
